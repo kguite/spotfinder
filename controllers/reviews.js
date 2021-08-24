c443a37921708be4a559be13b5115d1040d45b37
KatieMarie <kguite@gmail.com>
2021-08-24 00:41:29 -0700

    first commit

---
+++ controllers/reviews.js
@@ -0,0 +1,21 @@
+const Bikepath = require('../models/bikepath');
+const Review = require('../models/review');
+
+module.exports.createReview = async (req, res) => {
+	const bikepath = await Bikepath.findById(req.params.id);
+	const review = new Review(req.body.review);
+	review.author = req.user._id;
+	bikepath.reviews.push(review);
+	await review.save();
+	await bikepath.save();
+	req.flash('success', 'Created new review!');
+	res.redirect(`/bikepaths/${bikepath._id}`);
+}
+
+module.exports.deleteReview = async (req, res) => {
+	const { id, reviewId } = req.params;
+	await Bikepath.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+	await Review.findByIdAndDelete(reviewId);
+	req.flash('success', 'Successfully deleted review')
+	res.redirect(`/bikepaths/${id}`);
+}
---
+++ middleware.js
@@ -0,0 +1,54 @@
+const { bikepathSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Bikepath = require('./models/bikepath');
+const Review = require('./models/review');
+
+module.exports.isLoggedIn = (req, res, next) => {
+	if (!req.isAuthenticated()) {
+		req.session.returnTo = req.originalUrl
+		req.flash('error', 'You must be signed in first!');
+		return res.redirect('/login');
+	}
+	next();
+}
+
+module.exports.validateBikepath = (req, res, next) => {
+	const { error } = bikepathSchema.validate(req.body);
+	console.log(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',')
+		throw new ExpressError(msg, 400)
+	} else {
+		next();
+	}
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+	const { id } = req.params;
+	const bikepath = await Bikepath.findById(id);
+	if (!bikepath.author.equals(req.user._id)) {
+		req.flash('error', 'You do not have permission to do that!');
+		return res.redirect(`/bikepaths/${id}`);
+	}
+	next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+	const { id, reviewId } = req.params;
+	const review = await Review.findById(reviewId);
+	if (!review.author.equals(req.user._id)) {
+		req.flash('error', 'You do not have permission to do that!');
+		return res.redirect(`/bikepaths/${id}`);
+	}
+	next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+	const { error } = reviewSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',')
+		throw new ExpressError(msg, 400)
+	} else {
+		next();
+	}
+}
